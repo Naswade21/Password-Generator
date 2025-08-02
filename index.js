28fa68932bccc8ddf6bda6d7dbc59dd7a99e5138
Naswade21 <120067170+Naswade21@users.noreply.github.com>
2025-08-02 17:34:55 -0400

    Tested Button Onclick for adding text

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
+
+let pOne = document.getElementById("p-one")
+let pTwo = document.getElementById("p-two")
+
+function activate(){
+    pOne.textContent = "Euphoria"
+    pTwo.textContent = "Not Like Us"
+}
