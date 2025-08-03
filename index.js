a46225a1992b79ed33115c11c03a4dfe6f22f251
Naswade21 <120067170+Naswade21@users.noreply.github.com>
2025-08-03 00:28:46 -0400

    Works! but inefficient

--- index.js
+++ index.js
@@ -3,10 +3,53 @@ const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O"
 
 let pOne = document.getElementById("p-one")
 let pTwo = document.getElementById("p-two")
-let random = Math.floor(Math.random()*characters.length)
+
+
+function generateRandom(){
+   let random = Math.floor(Math.random()*characters.length)
+   let ran = characters[random]
+   return ran 
+}
+
+function fifteenWord(){
+    let wordA = generateRandom()
+    let wordB = generateRandom()
+    let wordC = generateRandom()
+    let wordD = generateRandom()
+    let wordE = generateRandom()
+    let wordF = generateRandom()
+    let wordG = generateRandom()
+    let wordH = generateRandom()
+    let wordI = generateRandom()
+    let wordJ = generateRandom()
+    let wordK = generateRandom()
+    let wordL = generateRandom()
+    let wordM = generateRandom()
+    let wordN = generateRandom()
+    let wordO = generateRandom()
+
+    let wordAA = generateRandom()
+    let wordBB = generateRandom()
+    let wordCC = generateRandom()
+    let wordDD = generateRandom()
+    let wordEE = generateRandom()
+    let wordFF = generateRandom()
+    let wordGG = generateRandom()
+    let wordHH = generateRandom()
+    let wordII = generateRandom()
+    let wordJJ = generateRandom()
+    let wordKK = generateRandom()
+    let wordLL = generateRandom()
+    let wordMM = generateRandom()
+    let wordNN = generateRandom()
+    let wordOO = generateRandom()
+
+    pOne.textContent = wordA + wordB + wordC + wordD + wordE + wordF + wordG + wordH + wordI
+    + wordJ + wordK + wordL + wordM + wordN + wordO
+    pTwo.textContent = wordAA + wordBB + wordCC + wordDD + wordEE + wordFF + wordGG + wordHH + wordII
+    + wordJJ + wordKK + wordLL + wordMM + wordNN + wordOO
+}
 
 function activate(){
-    pOne.textContent = "Euphoria"
-    pTwo.textContent = "Not Like Us"
-    console.log(random)
+  fifteenWord()
 }
