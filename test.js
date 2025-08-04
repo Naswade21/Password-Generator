40530b1164cbe826b299c730f3ec5ce43a74d0b8
Naswade21 <120067170+Naswade21@users.noreply.github.com>
2025-08-04 09:18:15 -0400

    Refactored my function and added copy plus test.js file

---
+++ test.js
@@ -0,0 +1,34 @@
+//Shortening mycode and testing if it works
+
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+let pOne = document.getElementById("p-one")
+let pTwo = document.getElementById("p-two")
+
+function generateRandomWord(e){
+    let password = "" //hold value generated
+    for (i = 0; i < e; i++){
+        let random = Math.floor(Math.random() * characters.length)
+        password += characters[random]
+    }
+    return password
+}
+
+function activate(){
+    pOne.textContent = generateRandomWord(15)
+    pTwo.textContent = generateRandomWord(15)
+}
+
+function getClipText(text){ //to copy text and save it, just follow Clipboard API - > writeText() Method give IT A Promise!
+    navigator.clipboard.writeText(pOne.textContent)
+    console.log("1st password copied was a success!")
+}
+
+function getClipText2(text){
+    navigator.clipboard.writeText(pTwo.textContent)
+    console.log("2nd password copied was a success!")
+}
+
+
+
