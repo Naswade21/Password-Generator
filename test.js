c79e99ab3465d337bc39dd145a1ba973f55a899f
Naswade21 <120067170+Naswade21@users.noreply.github.com>
2025-08-06 11:01:17 -0400

    Successfully implemented Remove symbols and Numbers switch. Also learned Boolean Constructer to be able to remove empty values after clearing values from character array using map and regex expression

--- test.js
+++ test.js
@@ -7,16 +7,42 @@ let pOne = document.getElementById("p-one")
 let pTwo = document.getElementById("p-two")
 let toggleSymbol = document.getElementById("a-switch")
 let toggleNumber = document.getElementById("b-switch")
+let noNumArray = characters.map(function removeNum(arr){
+    return arr.replace(/[0-9]/g, "") 
+   })
+let noSymArray = characters.map(function removeSym(arr){
+    return arr.replace(/[^a-zA-Z0-9]/g, "")
+   })
+let noSymNumArray = characters.map(function removeSymNum(arr){
+    return arr.replace(/[^a-z]/i, "")
+   })
 
-function noSymbols(){
-    
-}
 
 function generateRandomWord(e){
     let password = "" //hold value generated
-    for (i = 0; i < e; i++){
+    if(toggleNumber.checked === true && toggleSymbol.checked === true){ //AND or OR goes first in if else statements
+        for (i = 0; i < e; i++){
+            noSymNumArray = noSymNumArray.filter(Boolean)
+            let noSymNumRandom = Math.floor(Math.random()*noSymNumArray.length)
+            password += noSymNumArray[noSymNumRandom]
+        } console.log("Both are checked")
+    } else if (toggleSymbol.checked === true){
+         noSymArray = noSymArray.filter(Boolean)
+        for (i = 0; i < e; i++){
+            let noSymRandom = Math.floor(Math.random()*noSymArray.length)
+            password += noSymArray[noSymRandom]
+        } console.log("Remove Symbols is checked")
+    } else if (toggleNumber.checked === true){
+        noNumArray = noNumArray.filter(Boolean) //Boolean constructor turns values empty strings into falsy values and the filter method removes it
+        for (i = 0; i < e; i++){
+            let noNumRandom = Math.floor(Math.random()*noNumArray.length)
+            password += noNumArray[noNumRandom]
+        } console.log("Remove Numbers is checked")
+    } else {
+       for (i = 0; i < e; i++){
         let random = Math.floor(Math.random() * characters.length)
-        password += characters[random]
+       password += characters[random]
+   } console.log("Both aren't checked")
     }
     return password
 }
@@ -35,6 +61,8 @@ function getClipText2(text){
     navigator.clipboard.writeText(pTwo.textContent)
     console.log("2nd password copied was a success!")
 }
+
+
 
 
 
